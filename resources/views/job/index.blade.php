ef54778cf9d93dbdf109d7ec065f52f5956341ca
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-12-14 21:36:01 +0100

    feature Add radio button filtering

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -18,7 +18,34 @@
                     </div>
                 </div>
             </div>
-            <div>3</div>
+            <div>
+                <div class="mb-1 font-semibold">Experience</div>
+
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value=""
+                     @checked(!request('experience')) />
+                    <span class="ml-2">All</span>
+                </label>
+
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value="entry"
+                    @checked('entry' === request('experience')) />
+                    <span class="ml-2">Entry</span>
+                </label>
+
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value="intermediate" 
+                    @checked('intermediate' === request('experience')) />
+                    <span class="ml-2">Intermediate</span>
+                </label>
+
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value="senior"
+                    @checked('senior' === request('experience')) />
+                    <span class="ml-2">Senior</span>
+                </label>
+            </div>
+
             <div>4</div>
 
             <button class="w-full">Filter</button>
