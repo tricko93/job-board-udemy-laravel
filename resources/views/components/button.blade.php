5b4d7c4e34bfd3a9b4809cc94bf19b4cfed2a1b4
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-12-15 23:42:59 +0100

    refactor Refactor UI Components
    Gradient Background, Style Buttons, Add AlpineJS.

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+<button
+    {{ $attributes->class(['rounded-md border boder-slate-300 bg-white px-2.5 py-1.5 text-center text-sm font-semibold text-black shodow-sm hover:bg-slate-100']) }}>
+    {{ $slot }}
+</button>
