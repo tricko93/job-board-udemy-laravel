38a1c8eb7aa97f1997442e3a3eff2c5d2de4d903
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-12-15 22:42:45 +0100

    feature Add radio group component

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -13,40 +13,25 @@
                     <div class="mb-1 font-semibold">Salary</div>
 
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}"
+                            placeholder="From" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}"
+                            placeholder="To" />
                     </div>
                 </div>
+                <div>
+                    <div class="mb-1 font-semibold">Experience</div>
+
+                    <x-radio-group name="experience"
+                        :options="\App\Models\Job::$experience" />
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Category</div>
+
+                    <x-radio-group name="category"
+                        :options="\App\Models\Job::$category" />
+                </div>
             </div>
-            <div>
-                <div class="mb-1 font-semibold">Experience</div>
-
-                <label for="experience" class="mb-1 flex items-center">
-                    <input type="radio" name="experience" value=""
-                     @checked(!request('experience')) />
-                    <span class="ml-2">All</span>
-                </label>
-
-                <label for="experience" class="mb-1 flex items-center">
-                    <input type="radio" name="experience" value="entry"
-                    @checked('entry' === request('experience')) />
-                    <span class="ml-2">Entry</span>
-                </label>
-
-                <label for="experience" class="mb-1 flex items-center">
-                    <input type="radio" name="experience" value="intermediate" 
-                    @checked('intermediate' === request('experience')) />
-                    <span class="ml-2">Intermediate</span>
-                </label>
-
-                <label for="experience" class="mb-1 flex items-center">
-                    <input type="radio" name="experience" value="senior"
-                    @checked('senior' === request('experience')) />
-                    <span class="ml-2">Senior</span>
-                </label>
-            </div>
-
-            <div>4</div>
 
             <button class="w-full">Filter</button>
         </form>
