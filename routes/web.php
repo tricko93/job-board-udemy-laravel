b51ce268fbf706a29d8e3252b13d74c781ef7736
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-12-17 19:35:03 +0100

    feature Add JobAppController, Routing and Form

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobApplicationController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,8 @@ Route::resource('auth', AuthController::class)
 Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])
     ->name('auth.destroy');
+
+Route::middleware('auth')->group(function() {
+    Route::resource('job.application', JobApplicationController::class)
+        ->only(['create', 'store']);
+});
