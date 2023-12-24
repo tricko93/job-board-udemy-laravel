8fed505bd87d959bf7d835ca0d967fbe1bb5ea02
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-12-24 21:34:47 +0100

    refactor Extract label component

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -13,16 +13,16 @@
 			Your Job Application
 		</h2>
 
-		<form action="{{ route('job.application.store', $job) }}" method="POST" enctype="multipart/form-data">
+		<form action="{{ route('job.application.store', $job) }}" method="POST"
+			enctype="multipart/form-data">
 			@csrf
 			<div class="mb-4">
-				<label for="expected_salary"
-					class="mb-2 block text-sm font-medium text-slate-900">Expected Salary</label>
+				<x-label for="expected_salary" :required="true">Expected Salary</x-label>
 				<x-text-input type="number" name="expected_salary" />
 			</div>
 
 			<div class="mb-4">
-				<label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+				<x-label for="cv" :required="true">Upload CV</x-label>
 				<x-text-input type="file" name="cv"></x-text-input>
 			</div>
 
